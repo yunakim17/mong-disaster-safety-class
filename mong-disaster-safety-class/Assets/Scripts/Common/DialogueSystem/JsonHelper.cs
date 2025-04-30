4b62a0839f865389d7759bb5ec7cdfcfafedef05
seonjeong20 <kosam0307@gmail.com>
2025-04-30 13:53:48 +0900

    step1

---
+++ mong-disaster-safety-class/Assets/Scripts/Common/DialogueSystem/JsonHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(WrapJson(json));
+        return wrapper.Items;
+    }
+
+    private static string WrapJson(string rawJson)
+    {
+        return "{\"Items\":" + rawJson + "}";
+    }
+
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] Items;
+    }
+}
