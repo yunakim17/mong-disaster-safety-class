4cb650da48bef9870c10634f5c27dd853c306504
lnarim0823 <lnarim0823@gmail.com>
2025-06-10 15:47:03 +0900

    나의 배지 - UI 수정 및 점수 UI 추가 구현

--- mong-disaster-safety-class/Assets/Scripts/Common/BadgeReceiver.cs
+++ mong-disaster-safety-class/Assets/Scripts/Common/BadgeReceiver.cs
@@ -69,10 +69,31 @@ public class BadgeReceiver : MonoBehaviour
         {
             Debug.Log("스테이지 완료 처리 성공");
             isCompleted = true;
+
+            StartCoroutine(UpdateScore(requestData.user_id));
         }
         else
         {
             Debug.Log("스테이지 완료 처리 실패: " + request.error);
+        }
+    }
+
+    // 랭킹 점수 업데이트
+    IEnumerator UpdateScore(string userId)
+    {
+        string url = $"http://localhost:8000/ranking/update-badge?user_id={userId}";
+
+        UnityWebRequest request = UnityWebRequest.Put(url, "");
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("배지 점수 업데이트 성공");
+        }
+        else
+        {
+            Debug.Log("배지 점수 업데이트 실패: " + request.error);
         }
     }
 
