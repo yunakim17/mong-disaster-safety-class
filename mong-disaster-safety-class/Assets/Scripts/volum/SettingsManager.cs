455f03d5ff4a54b4cef30acc3ae60f86a7232070
JungJieun1 <109707180+JungJieun1@users.noreply.github.com>
2025-06-13 18:24:39 +0900

    볼륨 조절 패널

---
+++ mong-disaster-safety-class/Assets/Scripts/volum/SettingsManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static float GlobalVolume = 1f;
+
+    public GameObject settingsPanel;
+    public Slider volumeSlider;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            var bgm = FindObjectOfType<BGMPlayer>();
+            if (bgm != null)
+                audioSource = bgm.GetComponent<AudioSource>();
+        }
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.RemoveAllListeners();
+            volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+            volumeSlider.value = GlobalVolume;
+        }
+
+        if (audioSource != null)
+        {
+            audioSource.volume = GlobalVolume;
+        }
+    }
+
+    public void OnVolumeChanged(float value)
+    {
+        GlobalVolume = value;
+
+        if (audioSource != null)
+        {
+            audioSource.volume = value;
+        }
+    }
+
+    public void ToggleSettingsPanel()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(!settingsPanel.activeSelf);
+        }
+    }
+}
